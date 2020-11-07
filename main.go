044ac7b57c63ee332d596cf85d3eec5ef052f671
unblee <unblee@users.noreply.github.com>
2020-11-07 19:00:53 +0900

    Implement cli

--- main.go
+++ main.go
@@ -2,11 +2,74 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
+	"os"
 	"regexp"
 	"strings"
 )
+
+const helpMsg = `A tool for interactively viewing a dependency graph of go module.
+
+Usage: go mod graph | modgrapher OR modgrapher [file]
+
+[file] in args must be a result of 'go mod graph' command output.
+`
+
+var fs = flag.NewFlagSet("modgrapher", flag.ExitOnError)
+
+func init() {
+	fs.Usage = func() { fmt.Fprintln(os.Stderr, helpMsg) }
+}
+
+func main() {
+	if err := run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, helpMsg)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) (err error) {
+	if err := fs.Parse(args[1:]); err != nil {
+		return fmt.Errorf("failed to parse command args: %w", err)
+	}
+
+	if fs.NArg() > 1 {
+		return errors.New("too many arguments")
+	}
+
+	filename := fs.Arg(0)
+	var reader io.Reader
+	switch filename {
+	case "", "-":
+		reader = os.Stdin
+	default:
+		// If a file to be read is specified as an argument
+		f, err := os.Open(filename)
+		if err != nil {
+			return fmt.Errorf("%w", err)
+		}
+		defer func() {
+			if err = f.Close(); err != nil {
+				err = fmt.Errorf("%w", err)
+			}
+		}()
+		reader = f
+	}
+
+	graph, err := parseModGraph(reader)
+	if err != nil {
+		return fmt.Errorf("failed to parse input: %w", err)
+	}
+
+	fmt.Printf("%+v\n", *graph)
+
+	return nil
+}
 
 type Graph struct {
 	// The order of node and edge doesn't matter.
@@ -26,9 +89,6 @@ type Edge struct {
 	To   string
 }
 
-func main() {
-}
-
 func parseModGraph(r io.Reader) (*Graph, error) {
 	nodes := map[string]*Node{}
 	edges := []*Edge{}
@@ -39,7 +99,7 @@ func parseModGraph(r io.Reader) (*Graph, error) {
 
 		parentName, childName, err := getParentAndChildName(line)
 		if err != nil {
-			return nil, fmt.Errorf("the content contains a invalid line: %w", err)
+			return nil, fmt.Errorf("input content contains a invalid line: %w", err)
 		}
 
 		parentNode, ok := nodes[parentName]
